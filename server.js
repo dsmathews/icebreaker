8379ecca15a93ef9cef1610dab8dde97f30d9764
jjeng08 <jjeng08@users.noreply.github.com>
2018-12-10 15:40:41 -0500

    User can create and delete quizzes so only 1 is available at any time.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+require('dotenv').config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,6 +17,10 @@ require('./routes/api-routes')(app);
 
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/icebreaker");
+
+app.get('/', (req,res) => {
+  res.send(process.env.SECRET_KEY);
+})
 
 // Start the API server
 app.listen(PORT, function() {
